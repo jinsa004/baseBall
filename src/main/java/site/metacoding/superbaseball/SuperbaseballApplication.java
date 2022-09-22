ea2472b673fd0674dcfb256885516040e34e7255
jinsa <jinsa004@gmail.com>
2022-09-22 11:46:16 +0900

    기초세팅 완료

---
+++ src/main/java/site/metacoding/superbaseball/SuperbaseballApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.superbaseball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperbaseballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperbaseballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/metacoding/superbaseball/SuperbaseballApplicationTests.java
@@ -0,0 +1,13 @@
+package site.metacoding.superbaseball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperbaseballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
